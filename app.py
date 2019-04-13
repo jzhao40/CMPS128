d2e5f038cab7af8b0a4224fd16a9c048009fca00
jzhao40 <36348014+jzhao40@users.noreply.github.com>
2019-04-12 22:40:24 -0700

    Add files via upload
    added app.py and Flask.txt

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, redirect, render_template
+
+app = Flask(__name__)
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+	if request.method == 'GET':
+				return "Hello World!"
+	if request.method == 'POST':
+		return "method not supported", 405
+
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+			if request.method== 'GET':
+				return "GET Message Received"
+			if request.method== 'POST':
+				message =request.args.get('msg')
+				if message is None:
+					return "method not supported", 405
+				else:
+
+					return "POST message received: "+ message
+
+if __name__ == "__main__":
+	app.debug = True;
+	app.run(host = "0.0.0.0", port = 8080)
